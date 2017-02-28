35148f5ae76554f22ebbc9ca5862785c166ac34d
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-02-28 14:45:19 -0500

    Implement OpenMP (fix #2)
    Each thread has its own checksum, nonce partition and number buffer to
    compute the challenge. The first winner set 'done' to 'TRUE' and we can
    return the solution.
    
    There's no critical section on 'done' because whichever thread finish
    has a solution we can return.
    
    The only missing piece is to make the PRNG reentrant, then we could
    remove the critical section.

--- cscoin-solver.c
+++ cscoin-solver.c
@@ -5,6 +5,12 @@
 
 #include "contrib/mt19937-64/mt64.h"
 
+extern int
+omp_get_thread_num (void);
+
+extern int
+omp_get_num_threads (void);
+
 static gint
 gint64cmp_asc (const void *a, const void *b)
 {
@@ -30,16 +36,9 @@ cscoin_solve_challenge (gint         challenge_id,
                         const gchar *hash_prefix,
                         gint         nb_elements)
 {
-    g_autoptr (GChecksum) checksum = g_checksum_new (G_CHECKSUM_SHA256);
-    union {
-        guint8  digest[32];
-        guint64 seed;
-    } checksum_digest;
-    gsize checksum_digest_len = 32;
-    guint64 numbers[nb_elements];
-    gint nonce = 0;
-    gchar nonce_str[32];
     enum CSCoinChallengeType challenge_type;
+    gboolean done = FALSE;
+    gchar *ret = NULL;
 
     if (strcmp (challenge_name, "sorted_list") == 0)
     {
@@ -54,54 +53,85 @@ cscoin_solve_challenge (gint         challenge_id,
         g_return_val_if_reached (NULL);
     }
 
-    while (TRUE)
+    #pragma omp parallel
     {
-        g_snprintf (nonce_str, 32, "%d", nonce);
+        g_autoptr (GChecksum) checksum = g_checksum_new (G_CHECKSUM_SHA256);
+        union {
+            guint8  digest[32];
+            guint64 seed;
+        } checksum_digest;
+        gsize checksum_digest_len = 32;
+        guint64 numbers[nb_elements];
+        guint nonce;
+        gchar nonce_str[32];
 
-        g_checksum_update (checksum, (guchar*) last_solution_hash, 64);
-        g_checksum_update (checksum, (guchar*) nonce_str, strlen (nonce_str));
-        g_checksum_get_digest (checksum, checksum_digest.digest, &checksum_digest_len);
-        g_checksum_reset (checksum);
+        /* OpenMP partitionning */
+        guint nonce_from, nonce_to;
+        guint nonce_partition_size = G_MAXUINT / omp_get_num_threads ();
+        nonce_from = omp_get_thread_num () * nonce_partition_size;
+        nonce_to   = nonce_from + nonce_partition_size;
 
-        init_genrand64 (checksum_digest.seed);
-
-        gint i;
-        for (i = 0; i < nb_elements; i++)
+        for (nonce = nonce_from; nonce < nonce_to; nonce++)
         {
-            numbers[i] = genrand64_int64 ();
-        }
-
-        switch (challenge_type)
-        {
-            case CSCOIN_CHALLENGE_SORTED_LIST:
-                qsort (numbers, nb_elements, sizeof (guint64), gint64cmp_asc);
+            if (G_UNLIKELY (done))
+            {
                 break;
-            case CSCOIN_CHALLENGE_REVERSE_SORTED_LIST:
-                qsort (numbers, nb_elements, sizeof (guint64), gint64cmp_desc);
-                break;
-        }
+            }
 
-        gchar number_str[32];
-        for (i = 0; i < nb_elements; i++)
-        {
-            g_snprintf (number_str, 32, "%lu", numbers[i]);
-            g_checksum_update (checksum, (guchar*) number_str, strlen (number_str));
-        }
+            g_snprintf (nonce_str, 32, "%u", nonce);
 
-        const gchar * checksum_digest_str;
-        checksum_digest_str = g_checksum_get_string (checksum);
-
-        if (hash_prefix[0] == checksum_digest_str[0] &&
-            hash_prefix[1] == checksum_digest_str[1] &&
-            hash_prefix[2] == checksum_digest_str[2] &&
-            hash_prefix[3] == checksum_digest_str[3])
-        {
-            return g_strdup (nonce_str);
-        }
-        else
-        {
+            g_checksum_update (checksum, (guchar*) last_solution_hash, 64);
+            g_checksum_update (checksum, (guchar*) nonce_str, strlen (nonce_str));
+            g_checksum_get_digest (checksum, checksum_digest.digest, &checksum_digest_len);
             g_checksum_reset (checksum);
-            nonce++;
+
+            #pragma omp critical
+            {
+                init_genrand64 (checksum_digest.seed);
+
+                gint i;
+                for (i = 0; i < nb_elements; i++)
+                {
+                    numbers[i] = genrand64_int64 ();
+                }
+            }
+
+            switch (challenge_type)
+            {
+                case CSCOIN_CHALLENGE_SORTED_LIST:
+                    qsort (numbers, nb_elements, sizeof (guint64), gint64cmp_asc);
+                    break;
+                case CSCOIN_CHALLENGE_REVERSE_SORTED_LIST:
+                    qsort (numbers, nb_elements, sizeof (guint64), gint64cmp_desc);
+                    break;
+            }
+
+            gint i;
+            gchar number_str[32];
+            for (i = 0; i < nb_elements; i++)
+            {
+                g_snprintf (number_str, 32, "%lu", numbers[i]);
+                g_checksum_update (checksum, (guchar*) number_str, strlen (number_str));
+            }
+
+            const gchar * checksum_digest_str;
+            checksum_digest_str = g_checksum_get_string (checksum);
+
+            if (hash_prefix[0] == checksum_digest_str[0] &&
+                hash_prefix[1] == checksum_digest_str[1] &&
+                hash_prefix[2] == checksum_digest_str[2] &&
+                hash_prefix[3] == checksum_digest_str[3])
+            {
+                done = TRUE;
+                #pragma omp critical
+                ret = g_strdup (nonce_str);
+            }
+            else
+            {
+                g_checksum_reset (checksum);
+            }
         }
     }
+
+    return ret;
 }
