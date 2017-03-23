47e32a1f666aff0c4eb615e6ee7ce976e907fbb9
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-03-23 14:39:31 -0400

    Script for Docker

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 export DOCKER_HOST="tcp://miners.2017.csgames.org:2376"
 export DOCKER_TLS_VERIFY=1
-export DOCKER_CERT_PATH="cscert/"
+export DOCKER_CERT_PATH="$(pwd)/cscert/"
+
+docker --config . build .
+docker --config . create cscoin-miner
+docker --config . start cscoin-miner
